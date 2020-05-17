24d3814fc3c16bd779e802f4d9c7547a2647f99a
Phillip LeMaster <philalem21@gmail.com>
2020-05-17 17:22:58 -0400

    started the function to send user posts to followers

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +6,11 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.sendPostToFollowers = functions.firestore
+  .document("posts/{user-id}/user-posts/{post-id}")
+  .onCreate((snap, context) => {
+    // Get an object representing the document
+    // e.g. {'name': 'Marie', 'age': 66}
+    const newValue = snap.data();
+  });
