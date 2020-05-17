9b080648efe288ad97662ceb46b870d706c3ddf5
Phillip LeMaster <philalem21@gmail.com>
2020-05-17 18:53:23 -0400

    added new file for post functions

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,16 @@
+import "./user_post_functions";
+
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
 
 exports.sendPostToFollowers = functions.firestore
-  .document("posts/{user-id}/user-posts/{post-id}")
+  .document("posts/{userId}/user-posts/{postId}")
   .onCreate((snap, context) => {
-    // Get an object representing the document
-    // e.g. {'name': 'Marie', 'age': 66}
-    const newValue = snap.data();
+    createPostToFollowersBatchJobs(snap, context);
   });
