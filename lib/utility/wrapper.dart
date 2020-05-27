eef5ce84bfb06ceb53618dcae4b69c8b48176bc2
Phillip LeMaster <philalem21@gmail.com>
2020-05-27 12:00:33 -0400

    updated the auth flow

--- lib/utility/wrapper.dart
+++ lib/utility/wrapper.dart
@@ -1,20 +1,32 @@
 import 'package:creaid/home.dart';
 import 'package:creaid/utility/authenticate.dart';
 import 'package:creaid/utility/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final user = Provider.of<User>(context);
 
-    if(user == null) {
-      return Authenticate();
-    } else {
-      return Home();
-    }
+    return StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          FirebaseUser user = snapshot.data;
+          if (user == null) {
+            return Authenticate();
+          }
+          return Home();
+        } else {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
   }
 }
