49d680f5fdecdd166b3285e8c877914e284e0dd7
Phillip LeMaster <philalem21@gmail.com>
2020-06-01 09:08:29 -0400

    created add and remove cloud functions

---
+++ functions/userFollowFunctions.js
@@ -0,0 +1,27 @@
+const Firestore = require("@google-cloud/firestore");
+
+const firestore = new Firestore({
+  projectId: process.env.GCP_PROJECT,
+});
+
+exports.addUserToFollowers = async (snap, context) => {
+  const userId = context.params.userId;
+  const followedUserId = context.params.followingId;
+  return await firestore
+    .collection("user-info")
+    .doc(followedUserId)
+    .collection("followers")
+    .document(userId)
+    .addData({ uid: userId });
+};
+
+exports.removeUserFromFollowers = async (snap, context) => {
+  const userId = context.params.userId;
+  const followedUserId = context.params.followingId;
+  return await firestore
+    .collection("user-info")
+    .doc(followedUserId)
+    .collection("followers")
+    .document(userId)
+    .delete();
+};
