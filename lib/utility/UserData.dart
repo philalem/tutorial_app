810211130a489ab1324e74ac8d3c071d7da26076
Phillip LeMaster <philalem21@gmail.com>
2020-06-03 14:51:53 -0400

    updated the structure for data to be stored

--- lib/utility/UserData.dart
+++ lib/utility/UserData.dart
@@ -1,24 +1,15 @@
 class UserData {
   final String uid;
   String username;
-  String email;
   String name;
   String photoUrl;
   int numberFollowing;
   int numberFollowers;
-  List<String> interests;
-  List<String> followers;
-  List<String> following;
-  List<String> videos;
 
   UserData({
     this.uid,
     this.username,
-    this.email,
     this.name,
-    this.interests,
-    this.followers,
-    this.following,
     this.photoUrl,
     this.numberFollowers,
     this.numberFollowing,
