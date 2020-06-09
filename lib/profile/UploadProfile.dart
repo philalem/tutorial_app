3077aae34f428a848ef7cdee10a4f804b50d27a1
Phillip LeMaster <philalem21@gmail.com>
2020-06-09 10:41:17 -0400

    updated the login flow and the focus on the explore page

--- lib/profile/UploadProfile.dart
+++ lib/profile/UploadProfile.dart
@@ -4,7 +4,6 @@ import 'package:creaid/utility/user.dart';
 import 'package:creaid/utility/userDBService.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
@@ -23,51 +22,52 @@ class _UploadProfileState extends State<UploadProfile> {
     final user = Provider.of<User>(context);
 
     return Scaffold(
-        backgroundColor: Colors.white,
-        key: _scaffoldKey,
-        body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
-          child: Column(children: <Widget>[
-            RaisedButton(
+      backgroundColor: Colors.black87,
+      appBar: AppBar(
+        title: Text('Edit Your Profile Picture'),
+      ),
+      key: _scaffoldKey,
+      body: Column(children: <Widget>[
+        RaisedButton(
+            color: Colors.black,
+            child: Text(
+              'Choose Picture',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () {
+              chooseFile();
+            }),
+        _image != null
+            ? RaisedButton(
                 color: Colors.black,
                 child: Text(
-                  'Choose Picture',
+                  'Upload Picture',
                   style: TextStyle(color: Colors.white),
                 ),
+                onPressed: () async {
+                  uploadFile();
+                  print(_uploadedFileURL);
+                  if (_uploadedFileURL != null) {
+                    UserDbService(uid: user.uid)
+                        .updatePhotoUrl(_uploadedFileURL);
+                    Navigator.pop(context, true);
+                  } else {
+                    _showDialog();
+                  }
+                })
+            : new Container(),
+        SizedBox(height: 20.0),
+        _uploadedFileURL == null
+            ? RaisedButton(
+                color: Colors.black,
+                child: Text('Return to Profile',
+                    style: TextStyle(color: Colors.white)),
                 onPressed: () {
-                  chooseFile();
-                }),
-            _image != null
-                ? RaisedButton(
-                    color: Colors.black,
-                    child: Text(
-                      'Upload Picture',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () async {
-                      uploadFile();
-                      print(_uploadedFileURL);
-                      if (_uploadedFileURL != null) {
-                        UserDbService(uid: user.uid)
-                            .updatePhotoUrl(_uploadedFileURL);
-                        Navigator.pop(context, true);
-                      } else {
-                        _showDialog();
-                      }
-                    })
-                : new Container(),
-            SizedBox(height: 20.0),
-            _uploadedFileURL == null
-                ? RaisedButton(
-                    color: Colors.black,
-                    child: Text('Return to Profile',
-                        style: TextStyle(color: Colors.white)),
-                    onPressed: () {
-                      Navigator.pop(context, true);
-                    })
-                : new Container()
-          ]),
-        ));
+                  Navigator.pop(context, true);
+                })
+            : Container()
+      ]),
+    );
   }
 
   Future chooseFile() async {
