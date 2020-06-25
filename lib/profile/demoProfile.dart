073a1e5cf87312d8d8489d9c457cc30821819162
Phillip LeMaster <philalem21@gmail.com>
2020-06-25 10:52:19 -0400

    added the ability to pull the thumbnail to a users profile

--- lib/profile/demoProfile.dart
+++ lib/profile/demoProfile.dart
@@ -1,5 +1,7 @@
 import 'package:creaid/profile/editProfile.dart';
+import 'package:creaid/profile/post.dart';
 import 'package:creaid/profile/profilePhotoService.dart';
+import 'package:creaid/profile/profilePostsService.dart';
 import 'package:creaid/utility/UserData.dart';
 import 'package:creaid/utility/user.dart';
 import 'package:creaid/utility/userDBService.dart';
@@ -8,6 +10,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sticky_headers/sticky_headers.dart';
+import 'package:video_player/video_player.dart';
 
 void main() => runApp(MyApp());
 
@@ -349,7 +352,7 @@ class _ProfileFirstState extends State<ProfileFirst> {
                             color: Colors.white,
                             border: Border(
                                 bottom: BorderSide(
-                                    color: Colors.grey[200], width: 1)),
+                                    color: Colors.black, width: 0.1)),
                           ),
                           height: 40,
                           padding: EdgeInsets.symmetric(horizontal: 16.0),
@@ -413,7 +416,7 @@ class _ProfileFirstState extends State<ProfileFirst> {
                             color: Colors.white,
                             border: Border(
                                 bottom: BorderSide(
-                                    color: Colors.grey[200], width: 1)),
+                                    color: Colors.black, width: 0.1)),
                           ),
                           padding: EdgeInsets.symmetric(horizontal: 16.0),
                           alignment: Alignment.center,
@@ -430,38 +433,61 @@ class _ProfileFirstState extends State<ProfileFirst> {
                             SizedBox(
                               height: 30,
                             ),
-                            GridView.builder(
-                              physics: NeverScrollableScrollPhysics(),
-                              shrinkWrap: true,
-                              gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 3,
-                                crossAxisSpacing: 5.0,
-                                mainAxisSpacing: 5.0,
-                              ),
-                              itemCount: 18,
-                              itemBuilder: (context, index) {
-                                return Container(
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    child: Center(child: Text("Index: $index")),
-                                  ),
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey,
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    border: Border.all(
-                                        color: Colors.grey, width: 0.2),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.black54,
-                                        offset: Offset(-1.0, 1.0),
-                                        blurRadius: 1.0,
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                            ),
+                            StreamBuilder<Object>(
+                                stream: ProfilePostsService(uid: user.uid)
+                                    .getProfilePosts(),
+                                builder: (context, snapshot) {
+                                  if (!snapshot.hasData)
+                                    return Align(
+                                      alignment: Alignment.center,
+                                      child: CupertinoActivityIndicator(),
+                                    );
+                                  List<Post> posts = snapshot.data;
+                                  return GridView.builder(
+                                    physics: NeverScrollableScrollPhysics(),
+                                    shrinkWrap: true,
+                                    gridDelegate:
+                                        SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 3,
+                                      crossAxisSpacing: 5.0,
+                                      mainAxisSpacing: 5.0,
+                                    ),
+                                    itemCount: posts.length,
+                                    itemBuilder: (context, index) {
+                                      return Container(
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          child: posts[index].thumbnail !=
+                                                      null ||
+                                                  posts[index].thumbnail != ''
+                                              ? FadeInImage(
+                                                  image: NetworkImage(
+                                                      posts[index].thumbnail),
+                                                  placeholder: AssetImage(
+                                                      'assets/images/unknown-profile.png'),
+                                                )
+                                              : AssetImage(
+                                                  'assets/images/unknown-profile.png'),
+                                        ),
+                                        decoration: BoxDecoration(
+                                          color: Colors.grey,
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          border: Border.all(
+                                              color: Colors.grey, width: 0.2),
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: Colors.black54,
+                                              offset: Offset(-1.0, 1.0),
+                                              blurRadius: 1.0,
+                                            ),
+                                          ],
+                                        ),
+                                      );
+                                    },
+                                  );
+                                }),
                             SizedBox(
                               height: 30,
                             )
---
+++ lib/profile/post.dart
@@ -0,0 +1,6 @@
+class Post {
+  final String id;
+  final String thumbnail;
+
+  Post({this.id, this.thumbnail});
+}
