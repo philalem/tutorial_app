211e711dc2aa9903e0000a43669f930992964c99
Phillip LeMaster <philalem21@gmail.com>
2020-06-25 15:16:06 -0400

    generated thumbnail for post

--- lib/utility/postsDbService.dart
+++ lib/utility/postsDbService.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
 
 class PostsDbService {
   final String uid;
@@ -11,11 +13,20 @@ class PostsDbService {
       String title, String description, List<String> videoPaths) async {
     print('Adding post information...');
     var date = DateTime.now();
+    final thumbnail = await VideoThumbnail.thumbnailFile(
+      video: videoPaths[0],
+      thumbnailPath: (await getTemporaryDirectory()).path,
+      imageFormat: ImageFormat.PNG,
+      maxHeight:
+          64, // specify the height of the thumbnail, let the width auto-scaled to keep the source aspect ratio
+      quality: 75,
+    );
     var postData = {
       'title': title,
       'description': description,
       'videos': videoPaths,
       'number-likes': 0,
+      'thumbnail': thumbnail,
       'date': date,
     };
     DocumentReference userRef = await postsCollection
