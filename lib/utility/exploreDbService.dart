218322c5ef92d0879d3d3aaf9a4ae5ea7f055ac0
Phillip LeMaster <philalem21@gmail.com>
2020-07-21 20:20:38 -0400

    added the explore service and removed the profile collections

--- lib/utility/exploreDbService.dart
+++ lib/utility/exploreDbService.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:creaid/profile/post.dart';
 
 class ExploreDbService {
   final String uid;
@@ -6,4 +7,26 @@ class ExploreDbService {
       Firestore.instance.collection('explore');
 
   ExploreDbService({this.uid});
+
+  Post _mapToPost(DocumentSnapshot snapshot) {
+    return Post(
+      id: snapshot.documentID,
+      thumbnail: snapshot['thumbnail'],
+    );
+  }
+
+  Stream<List<Post>> getProfilePosts() {
+    Stream<QuerySnapshot> stream = exploreCollection
+        .document(uid)
+        .collection('user-posts')
+        .orderBy('date', descending: true)
+        .limit(20)
+        .snapshots()
+        .handleError((e) {
+      print(e);
+    });
+    return stream.map(
+      (snap) => snap.documents.map(_mapToPost).toList(),
+    );
+  }
 }
