dcb5c40b9416062a96699478b542c6b76147d4b0
Phillip LeMaster <philalem21@gmail.com>
2020-07-22 10:34:47 -0400

    added the bones for the explore cloud function

--- functions/feedFunctions.js
+++ functions/feedFunctions.js
@@ -6,54 +6,97 @@ const firestore = new Firestore({
 });
 
 exports.incrementNotifications = async (id) => {
-    return await firestore
+  return await firestore
     .collection("notifications")
     .doc(id)
     .update({ "new-notifications": FieldValue.increment(1) });
 };
 
 exports.sendLikeNotifications = async (snap, context) => {
-    const userId = context.params.uid;
-    const timestamp = new Date().getTime();
-    const name = snap.data().name;
-    exports.incrementNotifications(userId).catch((error) => {
-        console.log(error);
-      });
+  const userId = context.params.uid;
+  const timestamp = new Date().getTime();
+  const name = snap.data().name;
+  exports.incrementNotifications(userId).catch((error) => {
+    console.log(error);
+  });
 
-    return await firestore
-      .collection("notifications")
-      .doc(userId)
-      .collection("notifications")
-      .doc()
-      .set({
-        uid: userId,
-        name: name,
-        type: "Like notification",
-        comment: "",
-        date: timestamp,
-      });
+  return await firestore
+    .collection("notifications")
+    .doc(userId)
+    .collection("notifications")
+    .doc()
+    .set({
+      uid: userId,
+      name: name,
+      type: "like",
+      comment: "",
+      date: timestamp,
+    });
 };
 
 exports.sendCommentNotifications = async (snap, context) => {
-    const timestamp = new Date().getTime();
-    const name = snap.data().name;
-    const comment = snap.data().comment;
-    const userId = snap.data().uid;
+  const timestamp = new Date().getTime();
+  const name = snap.data().name;
+  const comment = snap.data().comment;
+  const userId = snap.data().uid;
 
-    exports.incrementNotifications(userId).catch((error) => {
-        console.log(error);
-      });
+  exports.incrementNotifications(userId).catch((error) => {
+    console.log(error);
+  });
 
-    return await firestore
-      .collection("notifications")
-      .doc(userId)
-      .collection("notifications")
-      .doc()
-      .set({
-        uid: userId,
-        name: name,
-        type: "Comment notification",
-        comment: comment,
-        date: timestamp,
-      });
+  return await firestore
+    .collection("notifications")
+    .doc(userId)
+    .collection("notifications")
+    .doc()
+    .set({
+      uid: userId,
+      name: name,
+      type: "comment",
+      comment: comment,
+      date: timestamp,
+    });
+};
+
+exports.updateExplorePosts = async (snap, context) => {
+  const userId = context.params.uid;
+  const videoId = context.params.videoId;
+  const feedId = context.params.feedId;
+  const timestamp = new Date().getTime();
+  const name = snap.data().name;
+  const currentExplorePosts = exports.getExplorePosts();
+  if (currentExplorePosts.length < 20) {
+    exports.addToExplorePosts(feedId, videoId);
+  }
+
+  return await firestore
+    .collection("notifications")
+    .doc(userId)
+    .collection("notifications")
+    .doc()
+    .set({
+      uid: userId,
+      name: name,
+      type: "like",
+      comment: "",
+      date: timestamp,
+    });
+};
+
+exports.getExplorePosts = async () => {
+  const topPosts = await firestore.collection("explore").get();
+  return topPosts.docs.map((postSnapshot) => ({
+    id: postSnapshot.id,
+    thumbnail: postSnapshot.get("thumbnail"),
+  }));
+};
+
+exports.addToExplorePosts = async (feedId, videoId) => {
+  const post = await firestore
+    .collection("posts")
+    .doc(feedId)
+    .collection("following-posts")
+    .doc(videoId)
+    .get();
+  return await firestore.collection("explore").doc(videoId).set(post);
 };
