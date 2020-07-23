48e7710ee513c19cf303b935c199d120c79f28fd
Phillip LeMaster <philalem21@gmail.com>
2020-07-23 15:11:44 -0400

    refactored the updateExplorPosts method

--- functions/feedFunctions.js
+++ functions/feedFunctions.js
@@ -59,35 +59,16 @@ exports.sendCommentNotifications = async (snap, context) => {
 };
 
 exports.updateExplorePosts = async (snap, context) => {
-  const userId = context.params.uid;
   const videoId = context.params.videoId;
   const feedId = context.params.feedId;
-  const timestamp = new Date().getTime();
-  const name = snap.data().name;
   const currentExplorePosts = await exports.getExplorePosts();
+  var lowestLikesPost = exports.findLowestLikedPost(currentExplorePosts);
+
   if (currentExplorePosts.length < 20) {
     return await exports.addToExplorePosts(feedId, videoId);
   }
-
-  // compare likes with current explore posts
-  // keep track of least liked post out of the explore
-  var lowestLikes = Number.MAX_SAFE_INTEGER;
-  var lowestLikesPost = { likes: Number.MAX_SAFE_INTEGER, id: null };
-  var i;
-  for (i = 0; i < currentExplorePosts.length; i++) {
-    if (currentExplorePosts[i].likes < lowestLikes) {
-      lowestLikesPost.likes = currentExplorePosts[i].likes;
-      lowestLikesPost.id = currentExplorePosts[i].id;
-    }
-  }
-
-  // get post in question
-  // if post in question has more likes, add it and remove the least liked post in explore
-  const postData = await exports.getPost(videoId);
-  if (postData.get("likes") > lowestLikesPost.likes) {
-    exports.removePostFromExplorePosts(lowestLikesPost.id);
-    exports.addToExplorePosts(postData.get(id));
-  }
+  await exports.addNewPostIfItHasMoreLikes(exports, videoId, lowestLikesPost);
+  return 0;
 };
 
 exports.getExplorePosts = async () => {
@@ -115,4 +96,24 @@ exports.addToExplorePosts = async (feedId, videoId) => {
 
 exports.removePostFromExplorePosts = async (videoId) => {
   return await firestore.collection("explore").doc(videoId).delete();
+};
+
+exports.findLowestLikedPost = (currentExplorePosts) => {
+  var lowestLikes = Number.MAX_SAFE_INTEGER;
+  var lowestLikesPost = { likes: Number.MAX_SAFE_INTEGER, id: null };
+  var i;
+  for (i = 0; i < currentExplorePosts.length; i++) {
+    if (currentExplorePosts[i].likes < lowestLikes) {
+      lowestLikesPost.likes = currentExplorePosts[i].likes;
+      lowestLikesPost.id = currentExplorePosts[i].id;
+    }
+  }
+  return lowestLikesPost;
+};
+exports.addNewPostIfItHasMoreLikes = async (videoId, lowestLikesPost) => {
+  const postData = await exports.getPost(videoId);
+  if (postData.get("likes") > lowestLikesPost.likes) {
+    exports.removePostFromExplorePosts(lowestLikesPost.id);
+    exports.addToExplorePosts(postData.get(id));
+  }
 };
